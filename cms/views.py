ae0da7c3dd622ab4e622690993241a547317db14
RaulCM <raulcanomontero@hotmail.com>
2017-03-27 22:53:50 +0200

    Ejercicio resuelto

--- cms/views.py
+++ cms/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from cms.models import Pages
+from django.http import HttpResponse
 
-# Create your views here.
+
+def show(request):
+    content = Pages.objects.all()
+    response = ""
+    for entry in content:
+        response = entry.name + ": " + entry.page + "<br>" + response
+    return HttpResponse(response)
+
+
+def entry(request, resource):
+    try:
+        entry = Pages.objects.get(name=resource)
+        return HttpResponse(entry.page)
+    except Pages.DoesNotExist:
+        return HttpResponseNotFound('<h1>' + resource + ' not found.</h1>')
